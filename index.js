f090d9f654e4d96ef0a45f5c7f4654ff998a61fa
ShonChassel <gershoni1234@gmail.com>
2023-12-06 18:52:32 +0200

    fix socet

--- index.js
+++ index.js
@@ -1,68 +1,63 @@
-import express from "express";
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express from "express"
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 import { Server as SocketServer } from 'socket.io';
-import http from 'http';
+import http from 'http'
 
-import authRoute from './routes/auth.js';
-import usersRoute from './routes/users.js';
-import tasksRoute from './routes/tasks.js';
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import tasksRoute from './routes/tasks.js'
 
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
-const app = express();
-const restServer = http.createServer(app);
-const ioServer = http.createServer();
-
-dotenv.config();
+const app = express()
+const server = http.createServer(app)
+dotenv.config()
 
 const connect = async () => {
+
     try {
         await mongoose.connect(process.env.MONGO);
-        console.log('connected to mongoDB');
+        console.log('connected to mongoDB')
     } catch (error) {
-        throw error;
+        throw error
     }
-};
+}
 
 mongoose.connection.on('disconnected', () => {
     console.log('mongoDB disconnected');
-});
+})
 
 mongoose.connection.on('connected', () => {
     console.log('mongoDB connected');
-});
+})
 
-// Middlewares
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
 
-app.use('/api/auth', authRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/tasks', tasksRoute);
+
+//? middlewares 
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+app.use('/api/auth', authRoute)
+app.use('/api/users', usersRoute)
+app.use('/api/tasks', tasksRoute)
+
+
 
 app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || 'Something went wrong';
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || 'Something went wrong'
     return res.status(errorStatus).json({
         success: false,
         status: errorStatus,
         message: errorMessage,
         stack: err.stack,
-    });
-});
+    })
+})
 
-const restPort = process.env.REST_PORT || 8800;
-const socketPort = process.env.SOCKET_PORT || 3003;
-
-restServer.listen(restPort, () => {
-    connect();
-    console.log(`REST API Server is running on port ${restPort}`);
-});
-
-const io = new SocketServer(ioServer, {
+const io = new SocketServer(server, {
     path: '/',
     cors: {
         origin: 'https://livecode-com.onrender.com',
@@ -70,13 +65,15 @@ const io = new SocketServer(ioServer, {
     }
 });
 
-io.attach(ioServer, {
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-});
+io.on("connection", (socket) => {
+    console.log(`conect ${socket.id}`);
 
-ioServer.listen(socketPort, () => {
-    console.log(`Socket.IO Server is running on port ${socketPort}`);
-});
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("receive_message", data)
+    })
+})
 
+server.listen(8800, () => {
+    connect()
+    console.log('connected to back!');
+})
